f52c8b7add2d05d75b1c9ae4b492e2a42ae76dda
Jeremy <jkgrub@gmail.com>
2021-09-28 18:23:32 -0700

    First Commit

---
+++ 01-java-fundamentals/01-Java First Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("MY hometown is Burbank, CA");
+    }
+}
